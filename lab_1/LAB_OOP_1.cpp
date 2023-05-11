be9f89a912f1244f5d76387570afd99437cc27cb
rrrilya <130274524+rrrilya@users.noreply.github.com>
2023-05-11 14:54:47 +0500

    Update LAB_OOP_1.cpp

--- lab_1/LAB_OOP_1.cpp
+++ lab_1/LAB_OOP_1.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
+
 using namespace std;
 
-class LinearEquation { 
+class LinearEquation { // объявление класса LinearEquation
 private:
-    double A; 
-    double B; 
+    double A; // поле first - коэффициент A
+    double B; // поле second - коэффициент B
 public:
-    LinearEquation(double a, double b) { 
+    LinearEquation(double a, double b) { // конструктор класса с двумя аргументами типа double
         A = a;
         B = b;
     }
-    double function(double x) { 
-        return A*x + B;
+    double root() { // метод root() вычисляет корень линейного уравнения и возвращает его значение
+        if (A == 0) { // проверка неравенства коэффициента A нулю
+            cout << "Ошибка: коэффициент A равен нулю" << endl; // вывод сообщения об ошибке
+            return -1; // возвращаем -1, так как корня нет
+        }
+        else {
+            return -B / A; // вычисляем корень линейного уравнения и возвращаем его значение
+        }
     }
 };
 
 int main() {
-    double a, b, x;
-    cout << "Ââåäèòå êîýôôèöèåíòû ëèíåéíîãî óðàâíåíèÿ (A è B): ";
+    double a, b;
+    cout << "Введите коэффициенты линейного уравнения (A и B): ";
     cin >> a >> b;
-    LinearEquation equation(a, b);
-    cout << "Ââåäèòå çíà÷åíèå x: ";
-    cin >> x;
-    double result = equation.function(x); 
-    cout << "Çíà÷åíèå ôóíêöèè äëÿ x = " << x << ": " << result << endl; 
+    LinearEquation equation(a, b); // создание объекта класса LinearEquation с заданными коэффициентами
+    double result = equation.root(); // вызов метода root() для вычисления корня линейного уравнения
+    if (result != -1) { // если метод root() вернул значение отличное от -1, то выводим результат
+        cout << "Корень линейного уравнения: " << result << endl;
+    }
     return 0;
 }
+
