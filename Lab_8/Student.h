6d3b3bc6fcb04d8f4e7cc365d79f1e7a3d764926
rrrilya <130274524+rrrilya@users.noreply.github.com>
2023-05-13 10:59:26 +0500

    Update Student.h

--- Lab_8/Student.h
+++ Lab_8/Student.h
@@ -6,8 +6,8 @@ class Student :
 {
 	string subject;
 	int mark;
-	friend istream& operator>>(istream& in, Student& s);			// ввод
-	friend ostream& operator<<(ostream& out, const Student& s);		// вывод
+	friend istream& operator>>(istream& in, Student& s);			
+	friend ostream& operator<<(ostream& out, const Student& s);		
 public:
 	Student();
 	Student(string, int, string, int);
