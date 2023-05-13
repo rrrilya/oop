4a2d55f3ababc74d13addb91c3bf21b8e78a4d93
rrrilya <130274524+rrrilya@users.noreply.github.com>
2023-05-13 10:54:33 +0500

    Update Dialog.h

--- Lab_8/Dialog.h
+++ Lab_8/Dialog.h
@@ -6,14 +6,14 @@ class Dialog :
 	public Vector
 {
 public:
-	Dialog(void);//контруктор public:
-	virtual ~Dialog(void);//деструктор
-	virtual void GetEvent(TEvent& event);//получить событие 
-	virtual int Execute();//главный цикл обработки событий 
-	virtual void HandleEvent (TEvent& event); //обработчик
-	virtual void ClearEvent (TEvent& event);//очистить событие 
-	int Valid();//проверка атрибута EndState
-	void EndExec();//обработка события «конец работы» 
+	Dialog(void);
+	virtual ~Dialog(void);
+	virtual void GetEvent(TEvent& event);
+	virtual int Execute();
+	virtual void HandleEvent (TEvent& event); 
+	virtual void ClearEvent (TEvent& event);
+	int Valid();
+	void EndExec();
 protected:	
 	int EndState;
 };
